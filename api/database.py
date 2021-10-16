20c6298070b49b143af9fb73ae33a2b5687ad6a1
norbusonam <nsonam1822@gmail.com>
2021-10-16 09:59:34 -0400

    database connection start

---
+++ api/database.py
@@ -0,0 +1,19 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# load env vars
+db_user = os.environ['DB_USER']
+db_pass = os.environ['DB_PASS']
+db_host = os.environ['DB_HOST']
+db_port = os.environ['DB_PORT']
+db_name = os.environ['DB_NAME']
+
+engine = create_engine(
+    f'postgresql+pg8000://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}',
+    connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
