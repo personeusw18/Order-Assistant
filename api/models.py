96bf8084903fc02365236a87d34ff93812d343cd
norbusonam <nsonam1822@gmail.com>
2021-10-16 10:00:34 -0400

    placeholder for models and schemas

---
+++ api/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+class Menu(Base):
+    __tablename__ = "menus"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
